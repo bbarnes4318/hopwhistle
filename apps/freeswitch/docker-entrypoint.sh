e158b09b7c768b18ba5017c012ea6b5a7c886aa4
Braden Barnes <bbarnes4318@gmail.com>
2025-11-24 07:46:44 -0500

    fix: use envsubst for SignalWire credentials in vars.xml

--- apps/freeswitch/docker-entrypoint.sh
+++ apps/freeswitch/docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 # Substitute environment variables in vars.xml if template exists
 if [ -f /etc/freeswitch/vars.xml.template ]; then
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN}' < /etc/freeswitch/vars.xml.template > /etc/freeswitch/vars.xml
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY}' < /etc/freeswitch/vars.xml.template > /etc/freeswitch/vars.xml
 elif [ -f /etc/freeswitch/vars.xml ]; then
     # If no template, do in-place substitution on the actual file
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN}' < /etc/freeswitch/vars.xml > /tmp/vars.xml.tmp
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY}' < /etc/freeswitch/vars.xml > /tmp/vars.xml.tmp
     mv /tmp/vars.xml.tmp /etc/freeswitch/vars.xml
 fi
 
